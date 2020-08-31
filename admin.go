f4a13b17853435d46262967118f40c3fd2175400
Charles Carlson <carlson.charles20@gmail.com>
2020-08-31 15:58:23 -0500

    refractored gohtml files

---
+++ admin.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+func adminLogin(w http.ResponseWriter, r *http.Request) {
+
+}
--- server.go
+++ server.go
@@ -1,25 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-var templates = template.Must(template.ParseGlob("static/*.go.html"))
+var templates = template.Must(template.ParseGlob("static/*.gohtml"))
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Home page")
 	templates.ExecuteTemplate(w, "home", nil)
 }
 func blogHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Blog page")
+	templates.ExecuteTemplate(w, "blog", nil)
 
 }
 func reviewHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Review page")
+	templates.ExecuteTemplate(w, "review", nil)
 
 }
 func methodHandler(w http.ResponseWriter, r *http.Request) {
-
+	fmt.Println("Methods page")
+	templates.ExecuteTemplate(w, "methods", nil)
 }
 
 //Middleware wrapper for mux.NewRouter()
@@ -35,7 +42,7 @@ func StartServer() *mux.Router {
 	router.HandleFunc("/", homeHandler)
 	router.HandleFunc("/blog", blogHandler)
 	router.HandleFunc("/review", reviewHandler)
-	router.HandleFunc("/method", methodHandler)
+	router.HandleFunc("/methods", methodHandler)
 	router.PathPrefix("/stylesheets/").Handler(http.StripPrefix("/stylesheets/", http.FileServer(http.Dir("stylesheets"))))
 	return router
 }
